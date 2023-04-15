b7f91ece9bfb01f85ac097dafed90fe66593a0c2
Kunwar Aditya <adityakunwar0147@gmail.com>
2023-04-15 07:23:55 +0530

    init: homepage

--- client/src/components/Layout.tsx
+++ client/src/components/Layout.tsx
@@ -2,8 +2,10 @@ import { Outlet } from 'react-router-dom';
 
 const Layout = () => {
   return (
-    <div className=''>
-      <Outlet />
+    <div className='flex overflow-hidden bg-no-repeat bg-center items-center justify-center p-4 h-screen bg-[url("./assets/back.webp")] bg-black/20 bg-blend-color'>
+      <div className='max-h-[45rem] flex items-center justify-center h-full max-w-[90rem] w-full'>
+        <Outlet />
+      </div>
     </div>
   );
 };
--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,4 +1,16 @@
+import logo from '../assets/logo.jpg';
+
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className='glassmorph w-full lg:w-[65%] h-full mx-auto rounded-md p-2'>
+      <div className='bg-white rounded-md p-1  '>
+        <img
+          src={logo}
+          alt='Adypu Logo'
+          className='rounded-md h-[8rem] w-auto object-contain mx-auto'
+        />
+      </div>
+    </div>
+  );
 };
 export default Home;
